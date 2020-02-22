2518ae2093a328beab2f36da88599db8ffa2cf3b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-22 01:00:47 +0000

    Deploy diogocezar/diogocezar.github.io to github.com/diogocezar/diogocezar.github.io.git:master

---
+++ precache-manifest.0badd20faba9504e7c1dcd9a2113406f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce34d0f1c7c62685d8b1",
+    "url": "/static/js/main.6baef4b5.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "5af1d9fedab16c360962",
+    "url": "/static/css/2.4e5b0780.chunk.css"
+  },
+  {
+    "revision": "5af1d9fedab16c360962",
+    "url": "/static/js/2.0f500dd8.chunk.js"
+  },
+  {
+    "revision": "93b35764533879e182429996c40acb0b",
+    "url": "/index.html"
+  }
+];
